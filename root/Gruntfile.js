1ef8b9b1fb50bdee3cd980e7bb0a693abac8a4ad
Jennifer Louthan <jenny@uncorkedstudios.com>
2014-01-13 13:33:06 -0800

    added comments

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,10 +1,15 @@
+// wrapper function
 module.exports = function(grunt){
+	// load all our Grunt plugins
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
+        // task configuration goes here
+
     });
 
+    // define the default task that executes when we run 'grunt' from inside the project
     grunt.registerTask('default', []);
 
 };
